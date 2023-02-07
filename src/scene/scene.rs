87ae92d8705577f8eb85ab4cca4a5d438c351a7b
Igor Pontes <igoppop@gmail.com>
2023-02-07 20:48:49 +0000

    ines file things

--- src/scene/scene.rs
+++ src/scene/scene.rs
@@ -1,35 +1,19 @@
-use std::fmt;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct Scene {
-    width: u32,
-    height: u32,
-    pixels: Option<u8>,
+    pub width: u32,
+    pub height: u32,
+    pub pixels: Option<u8>,
 }
 
 #[wasm_bindgen]
 impl Scene {
-    fn new() -> Scene {
+    pub fn new() -> Scene {
         Scene {
             width: 256,
             height: 240,
             pixels: None,
         }
-    }
-
-    fn get_index(&self, row: u32, column: u32) -> usize {
-        (row * self.width + column) as usize
-    }
-}
-
-impl fmt::Display for Scene {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                write!(f, "{x}")?;
-            }
-            write!(f, "\n")?;
-        }
-        Ok(())
     }
 }
